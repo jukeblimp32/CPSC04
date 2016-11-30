7ec3f49122b3ba5937948e9daca35ab23085ed4f
Talkov <ltalkov@pc106-mach.local>
2016-11-30 15:51:24 -0800

    New resizing of pages

--- Ocha/Ocha/ListingTableViewCell.swift
+++ Ocha/Ocha/ListingTableViewCell.swift
@@ -11,15 +11,60 @@ import UIKit
 class ListingTableViewCell: UITableViewCell {
     // MARK: Properties
     
-    @IBOutlet weak var propertyAddress: UILabel!
-    @IBOutlet weak var propertyDistance: UILabel!
-    @IBOutlet weak var propertyRent: UILabel!
-    @IBOutlet weak var propertyRooms: UILabel!
-    @IBOutlet weak var propertyImage: UIImageView!
+    let propertyAddress = UILabel()
+    let propertyDistance = UILabel()
+    let propertyRent = UILabel()
+    let propertyRooms = UILabel()
+    let propertyImage = UIImageView()
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        propertyAddress.font = UIFont(name: propertyRent.font.fontName, size: 15)
+        propertyAddress.textColor = UIColor.black
+        propertyAddress.frame = CGRect(x: (self.frame.width) * (5/100) , y: (self.frame.height) * (10/100), width: self.frame.width * (90/100) , height: 15)
+        self.addSubview(propertyAddress)
+        
+        propertyImage.frame = CGRect(x: (self.frame.width) * (5/100) , y: (self.frame.height) * (20/100), width: self.frame.width * (35/100) , height: self.frame.height * (75/100))
+        self.addSubview(propertyImage)
+        
+        
+        propertyRent.font = UIFont(name: propertyRent.font.fontName, size: 15)
+        propertyRent.textColor = UIColor.black
+        propertyRent.frame = CGRect(x: (self.frame.width) * (80/100), y: (self.frame.height) * (40/100), width: self.frame.width * (15/100), height: 15)
+        self.addSubview(propertyRent)
+        
+        propertyRooms.font = UIFont(name: propertyRent.font.fontName, size: 15)
+        propertyRooms.textColor = UIColor.black
+        propertyRooms.frame = CGRect(x: (self.frame.width) * (80/100), y: (self.frame.height) * (60/100), width: self.frame.width * (15/100), height: 15)
+        self.addSubview(propertyRooms)
+        
+        propertyDistance.font = UIFont(name: propertyRent.font.fontName, size: 15)
+        propertyDistance.textColor = UIColor.black
+        propertyDistance.frame = CGRect(x: (self.frame.width) * (80/100), y: (self.frame.height) * (80/100), width: self.frame.width * (15/100), height: 15)
+        self.addSubview(propertyDistance)
+        
+        let rentLabel = UILabel()
+        rentLabel.text = "Rent per Month:"
+        rentLabel.font = UIFont(name: propertyRent.font.fontName, size: 15)
+        rentLabel.textColor = UIColor.black
+        rentLabel.frame = CGRect(x: (self.frame.width) * (45/100), y: (self.frame.height) * (40/100), width: self.frame.width * (35/100), height: 15)
+        self.addSubview(rentLabel)
+        
+        let roomLabel = UILabel()
+        roomLabel.text = "Bedrooms:"
+        roomLabel.font = UIFont(name: propertyRent.font.fontName, size: 15)
+        roomLabel.textColor = UIColor.black
+        roomLabel.frame = CGRect(x: (self.frame.width) * (45/100), y: (self.frame.height) * (60/100), width: self.frame.width * (35/100), height: 15)
+        self.addSubview(roomLabel)
+        
+        let distanceLabel = UILabel()
+        distanceLabel.text = "Miles to GU:"
+        distanceLabel.font = UIFont(name: propertyRent.font.fontName, size: 15)
+        distanceLabel.textColor = UIColor.black
+        distanceLabel.frame = CGRect(x: (self.frame.width) * (45/100), y: (self.frame.height) * (80/100), width: self.frame.width * (35/100), height: 15)
+        self.addSubview(distanceLabel)
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
